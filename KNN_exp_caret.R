af74959735499415823ef42497bc1546ce5e7a78
greg3000 <ghaigh@gmail.com>
2015-07-31 15:52:32 +0100

    added KNN_exp_caret.R

---
+++ KNN_exp_caret.R
@@ -0,0 +1,35 @@
+library(class) #needed for the islr knn approach
+library(caret)
+library(ISLR)
+
+setwd("~/Documents/R/fp_targeting/")
+data = read.csv("KNN_Exp.csv")
+attach(data)
+plot(x,y, col=as.factor(age))
+
+#standardise the data
+
+data$x=scale(data$x)
+data$y=scale(data$y)
+
+
+
+#plot the data again
+plot(data$x,data$y, col=as.factor(data$age))
+
+#create testing and training sets
+inTrain <- createDataPartition(y=age, p=0.75, list=FALSE)
+training <- data[inTrain,]
+testing <- data[-inTrain,]
+
+
+
+#run the knn prediction
+
+#modelFit <- train(age ~., data=training, method= "knn")
+modelFit <- train(as.factor(age) ~., data=training , method= "knn")
+modelFit$finalModel
+predictions <- predict(modelFit, newdata=testing)
+predictions
+confusionMatrix(predictions, testing$age)
+
